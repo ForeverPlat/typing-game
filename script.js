074384d84173218aa3a549f21832500008b4ab48
foreverplat <luqman.o.ajani@gmail.com>
2025-02-27 22:24:16 -0700

    made typing update the color of the text

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
-var typingText = document.getElementById("typing-test-text").textContent;
+// var typingText = document.getElementById("typing-test-text").textContent;
+
+//later us an api to generate random strings
+var typingText = "lorem ipsum dolor sit ame";
+
+var wordList = typingText.split(" ");
+console.log(wordList);
+
+for (var i = 0; i < wordList.length; i++) {
+
+    console.log(wordList[i]);
+
+    document.getElementById("typing-test-text").innerHTML += "<span class='word'></span>";
+
+    var words = document.querySelectorAll(".word")
+    var currWord = words[words.length - 1];
+
+    var letterList = wordList[i].split("");
+
+    for (var j = 0; j < letterList.length; j++) {
+
+        console.log(letterList[j])
+        currWord.innerHTML += "<span class='letter'>" + letterList[j] + "</span>";
+    }
+
+    if (i < wordList.length - 1) {
+        document.getElementById("typing-test-text").innerHTML += "<span class='letter'>&nbsp;</span>";
+    }
+
+}
+
+console.log(document.querySelectorAll(".letter"))
+
+var letters = document.querySelectorAll(".letter");
+
+// for (i; i < length; i++) {
+//     typingText.innerHTML += "<span>" + splitText[i] + "</span>";
+// }
+
 
 var count = 0;
 
+function resetTypeTest() {
+    count = 0;
+    console.log("count: " + count)
+    var letter = document.querySelectorAll(".letter");
+
+    // change to for each later
+
+    for (var letter of letters) {
+        letter.style="color: #363a43;"
+    }
+
+}
+
 //each key stroke movest the index by one
 // if the keystroke does not match the previous letter turn that letter red
+
+//might need a wrong counter
+
 
 document.addEventListener("keydown", event => {
 
@@ -17,16 +71,25 @@ document.addEventListener("keydown", event => {
 
         count--;
 
+        letters[count].style="color: #363a43;"
+
+
     } else if (currChar == typedChar) {
 
-
-
-        console.log("white")
+        letters[count].style="color:rgb(209, 209, 202);"
         count++;
 
     } else {
 
-        console.log("red")
+        if (letters[count] == "&nbsp;") {
+            letters[count].style="background-color:rgb(219, 108, 108);"
+
+        } else {
+            letters[count].style="color:rgb(219, 108, 108);"
+
+        }
+
+
         count++;
         
     }
@@ -35,5 +98,4 @@ document.addEventListener("keydown", event => {
 
 });
 
-//split each character into a value in an array
-//every time the keystroke equals the value the index goes up by one
+
